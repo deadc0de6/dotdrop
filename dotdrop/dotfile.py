12df68bf70d7f47e1b5fc9aa408b90b66834617e
deadc0de6 <info@deadc0de.ch>
2017-03-15 20:55:54 +0100

    comment

--- dotdrop/dotfile.py
+++ dotdrop/dotfile.py
@@ -8,9 +8,9 @@ represents a dotfile in dotdrop
 class Dotfile:
 
     def __init__(self, key, dst, src):
-        self.key = key
-        self.dst = dst
-        self.src = src
+        self.key = key # key of dotfile in the config
+        self.dst = dst # where to install this dotfile
+        self.src = src # stored dotfile in dotdrop
 
     def __str__(self):
         string = 'key:%s, src: %s, dst: %s' % (self.key,
