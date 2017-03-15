65db6e25fd226b82a0b42b44d2aed2aac2effa26
deadc0de6 <info@deadc0de.ch>
2017-03-15 18:27:37 +0100

    stop test script one first error

--- tests.sh
+++ tests.sh
@@ -2,6 +2,9 @@
 # author: deadc0de6 (https://github.com/deadc0de6)
 # Copyright (c) 2017, deadc0de6
 
+# stop on first error
+set -ev
+
 pep8 dotdrop/
 pep8 tests/
 PYTHONPATH=dotdrop nosetests -w tests
