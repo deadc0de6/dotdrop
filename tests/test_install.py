17426ca9e5e984f06ee6f26d8974e204a5ccbde8
deadc0de6 <info@deadc0de.ch>
2017-03-15 21:44:25 +0100

    adding test placeholders

---
+++ tests/test_install.py
@@ -0,0 +1,22 @@
+"""
+author: deadc0de6 (https://github.com/deadc0de6)
+Copyright (c) 2017, deadc0de6
+basic unittest for the install function
+"""
+
+import unittest
+
+
+class TestInstall(unittest.TestCase):
+
+    def test_install(self):
+        '''Test the install function'''
+        # TODO
+        self.assertTrue(True)
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
