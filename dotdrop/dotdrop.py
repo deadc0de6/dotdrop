9781877556ab68b91466dcccd862905a9daaa955
deadc0de6 <info@deadc0de.ch>
2017-09-08 13:52:39 +0200

    bump version

--- dotdrop/dotdrop.py
+++ dotdrop/dotdrop.py
@@ -15,7 +15,7 @@ from installer import Installer
 from dotfile import Dotfile
 from config import Cfg
 
-VERSION = '0.3'
+VERSION = '0.4'
 CUR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 LOG = Logger()
 HOSTNAME = os.uname()[1]
