50a7b680024587facddf235defc461ca43bdbb60
Maëlan <maelan44@msn.com>
2019-08-13 18:35:29 +0200

    add option to disable auto-updates

--- dotdrop.sh
+++ dotdrop.sh
@@ -24,8 +24,10 @@ sub="dotdrop"
 # pivot
 cd "${cur}" || { echo "Directory \"${cur}\" doesn't exist, aborting." && exit 1; }
 # init/update the submodule
-git submodule update --init --recursive
-git submodule update --remote dotdrop
+if [ "${DOTDROP_AUTOUPDATE-yes}" = yes ] ; then
+  git submodule update --init --recursive
+  git submodule update --remote dotdrop
+fi
 # launch dotdrop
 PYTHONPATH=dotdrop python3 -m dotdrop.dotdrop "${args[@]}"
 ret="$?"
