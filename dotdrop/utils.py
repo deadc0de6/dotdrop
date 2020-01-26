0e421c7908ad974b141bba799e36b351a0ce17cc
deadc0de6 <info@deadc0de.ch>
2020-01-26 19:54:25 +0100

    default to unified diff

--- dotdrop/utils.py
+++ dotdrop/utils.py
@@ -70,10 +70,10 @@ def shell(cmd, debug=False):
 
 
 def diff(original, modified, raw=True,
-         diff_cmd='diff -r {0} {1}', debug=False):
+         diff_cmd='', debug=False):
     """compare two files"""
     if not diff_cmd:
-        diff_cmd = 'diff -r {0} {1}'
+        diff_cmd = 'diff -r -u {0} {1}'
 
     replacements = {
         "{0}": original,
