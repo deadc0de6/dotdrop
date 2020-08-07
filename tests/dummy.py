ff6fad8d84aab264d8cb3934ca05492f1794b7a2
deadc0de6 <info@deadc0de.ch>
2020-08-07 14:59:26 +0200

    adding a test for init

---
+++ tests/dummy.py
@@ -0,0 +1,22 @@
+"""
+author: deadc0de6 (https://github.com/deadc0de6)
+Copyright (c) 2017, deadc0de6
+basic unittest for the import function
+"""
+
+
+import unittest
+import dotdrop
+
+
+class TestDummy(unittest.TestCase):
+
+    dotdrop.main()
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
