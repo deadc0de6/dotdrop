ae504f947338c25f87a28cd1d1ad28d8a1cb3182
deadc0de6 <info@deadc0de.ch>
2020-09-24 07:12:17 +0200

    fix import issue for #260

--- dotdrop/__init__.py
+++ dotdrop/__init__.py
@@ -2,3 +2,12 @@
 author: deadc0de6 (https://github.com/deadc0de6)
 Copyright (c) 2017, deadc0de6
 """
+
+import sys
+
+
+def main():
+    import dotdrop.dotdrop
+    if dotdrop.dotdrop.main():
+        sys.exit(0)
+    sys.exit(1)
