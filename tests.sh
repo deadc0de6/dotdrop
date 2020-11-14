15617ceaea6e7a19ecccd118d9ef5ad8aa81db7f
deadc0de6 <info@deadc0de.ch>
2020-11-14 12:12:47 +0100

    enable parallel testing

--- tests.sh
+++ tests.sh
@@ -37,8 +37,8 @@ if [ -z ${GITHUB_WORKFLOW} ]; then
   #PYTHONPATH="dotdrop" ${nosebin} --processes=0 --with-coverage --cover-package=dotdrop
   PYTHONPATH="dotdrop" ${nosebin} -s --processes=-1 --with-coverage --cover-package=dotdrop
 else
-  #PYTHONPATH="dotdrop" ${nosebin} --processes=-1 --with-coverage --cover-package=dotdrop
-  PYTHONPATH="dotdrop" ${nosebin} --processes=0 --with-coverage --cover-package=dotdrop
+  PYTHONPATH="dotdrop" ${nosebin} --processes=-1 --with-coverage --cover-package=dotdrop
+  #PYTHONPATH="dotdrop" ${nosebin} --processes=0 --with-coverage --cover-package=dotdrop
 fi
 #PYTHONPATH="dotdrop" python3 -m pytest tests
 
