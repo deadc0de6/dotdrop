fcbe750d42922abbe178c81b6406df534f9c43db
deadc0de6 <info@deadc0de.ch>
2020-11-15 10:58:01 +0100

    more tests

---
+++ tests-ng/chmod-more.sh
@@ -0,0 +1,127 @@
+#!/usr/bin/env bash
+# author: deadc0de6 (https://github.com/deadc0de6)
+# Copyright (c) 2020, deadc0de6
+#
+# test chmod on import
+# with files and directories
+# with different link
+#
+
+# exit on first error
+set -e
+
+# all this crap to get current path
+rl="readlink -f"
+if ! ${rl} "${0}" >/dev/null 2>&1; then
+  rl="realpath"
+
+  if ! hash ${rl}; then
+    echo "\"${rl}\" not found !" && exit 1
+  fi
+fi
+cur=$(dirname "$(${rl} "${0}")")
+
+#hash dotdrop >/dev/null 2>&1
+#[ "$?" != "0" ] && echo "install dotdrop to run tests" && exit 1
+
+#echo "called with ${1}"
+
+# dotdrop path can be pass as argument
+ddpath="${cur}/../"
+[ "${1}" != "" ] && ddpath="${1}"
+[ ! -d ${ddpath} ] && echo "ddpath \"${ddpath}\" is not a directory" && exit 1
+
+export PYTHONPATH="${ddpath}:${PYTHONPATH}"
+bin="python3 -m dotdrop.dotdrop"
+hash coverage 2>/dev/null && bin="coverage run -a --source=dotdrop -m dotdrop.dotdrop" || true
+
+echo "dotdrop path: ${ddpath}"
+echo "pythonpath: ${PYTHONPATH}"
+
+# get the helpers
+source ${cur}/helpers
+
+echo -e "$(tput setaf 6)==> RUNNING $(basename $BASH_SOURCE) <==$(tput sgr0)"
+
+################################################################
+# this is the test
+################################################################
+
+# $1 path
+# $2 rights
+has_rights()
+{
+  echo "testing ${1} is ${2}"
+  [ ! -e "$1" ] && echo "`basename $1` does not exist" && exit 1
+  local mode=`stat -L -c '%a' "$1"`
+  [ "${mode}" != "$2" ] && echo "bad mode for `basename $1` (${mode} instead of ${2})" && exit 1
+  true
+}
+
+# $1 file
+chmod_to_umask()
+{
+  u=`umask`
+  u=`echo ${u} | sed 's/^0*//'`
+  if [ -d ${1} ]; then
+    v=$((777 - u))
+  else
+    v=$((666 - u))
+  fi
+  chmod ${v} ${1}
+}
+
+# the dotfile source
+tmps=`mktemp -d --suffix='-dotdrop-tests' || mktemp -d`
+mkdir -p ${tmps}/dotfiles
+# the dotfile destination
+tmpd=`mktemp -d --suffix='-dotdrop-tests' || mktemp -d`
+#echo "dotfile destination: ${tmpd}"
+
+# create the dotfiles
+f1="${tmpd}/f1"
+touch ${f1}
+chmod 777 ${f1}
+stat -c '%a' ${f1}
+
+f2="${tmpd}/f2"
+touch ${f2}
+chmod 644 ${f2}
+stat -c '%a' ${f2}
+
+toimport="${f1} ${f2}"
+
+# create the config file
+cfg="${tmps}/config.yaml"
+
+cat > ${cfg} << _EOF
+config:
+  backup: true
+  create: true
+  dotpath: dotfiles
+dotfiles:
+profiles:
+_EOF
+#cat ${cfg}
+
+# import without --preserve-mode
+for i in ${toimport}; do
+  stat -c '%a' ${i}
+  cd ${ddpath} | ${bin} import -c ${cfg} -f -p p1 -V ${i}
+done
+
+cat ${cfg}
+
+has_rights "${tmpd}/f1" "777"
+has_rights "${tmps}/dotfiles/${tmpd}/f1" "777"
+has_rights "${tmpd}/f2" "644"
+has_rights "${tmps}/dotfiles/${tmpd}/f2" "644"
+
+# install
+cd ${ddpath} | ${bin} install -c ${cfg} -f -p p1 -V | grep '0 dotfile(s) installed' || (echo "should not install" && exit 1)
+
+## CLEANING
+rm -rf ${tmps} ${tmpd}
+
+echo "OK"
+exit 0
