2c7ac514e09b9dae92347ab4102554e6b19a5094
deadc0de6 <info@deadc0de.ch>
2021-09-29 08:12:30 +0200

    handle python3 as python

--- dotdrop.sh
+++ dotdrop.sh
@@ -23,15 +23,24 @@ sub="dotdrop"
 
 # pivot
 cd "${cur}" || { echo "Directory \"${cur}\" doesn't exist, aborting." && exit 1; }
+
 # init/update the submodule
 if [ "${DOTDROP_AUTOUPDATE-yes}" = yes ] ; then
   git submodule update --init --recursive
   git submodule update --remote dotdrop
 fi
+
+# check python executable
+pybin="python3"
+hash ${pybin} 2>/dev/null || pybin="python"
+[[ "`${pybin} -V 2>&1`" =~ "Python 3" ]] || { echo "install Python 3" && exit 1; }
+
 # launch dotdrop
-PYTHONPATH=dotdrop python3 -m dotdrop.dotdrop "${args[@]}"
+PYTHONPATH=dotdrop ${pybin} -m dotdrop.dotdrop "${args[@]}"
 ret="$?"
+
 # pivot back
 cd "${opwd}" || { echo "Directory \"${opwd}\" doesn't exist, aborting." && exit 1; }
+
 # exit with dotdrop exit code
 exit ${ret}
