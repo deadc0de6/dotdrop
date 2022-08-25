565dad58c65e0635e1f061e2690a1d0f38103463
deadc0de6 <info@deadc0de.ch>
2022-08-25 23:30:14 +0200

    no more python 3.5

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(
     python_requires=REQUIRES_PYTHON,
     classifiers=[
             'Development Status :: 5 - Production/Stable',
-            'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
