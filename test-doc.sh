b3e79da7f9b87d55bbe0f7e9b4c20fb1dfeb6c51
deadc0de6 <info@deadc0de.ch>
2023-01-18 07:08:00 +0100

    change dead link check tool

--- test-doc.sh
+++ test-doc.sh
@@ -2,18 +2,34 @@
 # author: deadc0de6 (https://github.com/deadc0de6)
 # Copyright (c) 2022, deadc0de6
 
-## test the doc with remark
-## https://github.com/remarkjs/remark-validate-links
+## test the doc with linkcheckMarkdown
+## pip install --user linkcheckmd
 set +e
-which remark >/dev/null 2>&1
+which linkcheckMarkdown >/dev/null 2>&1
 r="$?"
 set -e
 if [ "$r" != "0" ]; then
-  echo "[WARNING] install \"remark\" to test the doc"
-else
-  remark -f -u validate-links docs/
-  remark -f -u validate-links *.md
+  echo "[ERROR] install \"linkcheckMarkdown\" to test for dead links"
+  exit 1
 fi
+find . -type f -iname '*.md' | while read line; do
+  echo "checking links in \"${line}\""
+  linkcheckMarkdown ${line}
+done
+
+
+### test the doc with remark
+### https://github.com/remarkjs/remark-validate-links
+#set +e
+#which remark >/dev/null 2>&1
+#r="$?"
+#set -e
+#if [ "$r" != "0" ]; then
+#  echo "[WARNING] install \"remark\" to test the doc"
+#else
+#  remark -f -u validate-links docs/
+#  remark -f -u validate-links *.md
+#fi
 
 ### test the doc with markdown-link-check
 ### https://github.com/tcort/markdown-link-check
