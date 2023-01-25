7d4fc8040213fa4aee3d615591f3e63a6854493c
deadc0de6 <info@deadc0de.ch>
2023-01-25 21:20:07 +0100

    test if version.py and manpage differ

--- tests.sh
+++ tests.sh
@@ -16,6 +16,15 @@ if ! ${rl} "${0}" >/dev/null 2>&1; then
 fi
 cur=`dirname $(${rl} "${0}")`
 
+# make sure both version.py and manpage dotdrop.1 are in sync
+dotdrop_version=$(grep version dotdrop/version.py | sed 's/^.*= .\(.*\).$/\1/g')
+man_version=$(grep '^\.TH' manpage/dotdrop.1  | sed 's/^.*"dotdrop-\(.*\)\" "Save your.*$/\1/g')
+if [ "${dotdrop_version}" != "${man_version}" ]; then
+  echo "ERROR version.py (${dotdrop_version}) and manpage (${man_version}) differ!"
+  exit 1
+fi
+echo "current version ${dotdrop_version}"
+
 # test syntax
 echo "checking syntax..."
 ${cur}/test-syntax.sh
