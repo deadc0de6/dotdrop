2af2aecfdaf4e0d39b5e70940ca7bccce5931640
deadc0de6 <info@deadc0de.ch>
2023-01-29 21:46:19 +0100

    linting

--- scripts/check-unittests.sh
+++ scripts/check-unittests.sh
@@ -5,16 +5,6 @@
 # stop on first error
 set -e
 
-rl="readlink -f"
-if ! ${rl} "${0}" >/dev/null 2>&1; then
-  rl="realpath"
-
-  if ! hash ${rl}; then
-    echo "\"${rl}\" not found!" && exit 1
-  fi
-fi
-cur=$(dirname "$(${rl} "${0}")")
-
 if [ -n "${DOTDROP_WORKERS}" ]; then
   unset DOTDROP_WORKERS
   echo "DISABLE workers for unittests"
