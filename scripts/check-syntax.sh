bb59b3a5ae8b38e40ff580fba42115e5427f024c
deadc0de6 <info@deadc0de.ch>
2023-02-05 17:11:43 +0100

    change test order

--- scripts/check-syntax.sh
+++ scripts/check-syntax.sh
@@ -35,41 +35,6 @@ fi
 echo "=> pyflakes version:"
 pyflakes --version
 
-# check shell scripts
-# SC2002: Useless cat
-# SC2126: Consider using grep -c instead of grep|wc -l
-# SC2129: Consider using { cmd1; cmd2; } >> file instead of individual redirects
-# SC2181: Check exit code directly with e.g. 'if mycmd;', not indirectly with $?
-# SC1004: This backslash+linefeed is literal. Break outside single quotes if you just want to break the line
-echo "--------------------------------------"
-echo "checking shell scripts with shellcheck"
-echo "--------------------------------------"
-find . -iname '*.sh' | while read -r script; do
-  echo "checking ${script}"
-  shellcheck -x \
-    -e SC2002 \
-    -e SC2126 \
-    -e SC2129 \
-    -e SC2181 \
-    -e SC1004 \
-    -e SC1117 \
-    -e SC2230 \
-    "${script}"
-done
-
-# check other python scripts
-echo "-----------------------------------------"
-echo "checking other python scripts with pylint"
-echo "-----------------------------------------"
-find . -name "*.py" -not -path "./dotdrop/*" | while read -r script; do
-  echo "checking ${script}"
-  pylint -sn \
-    --disable=R0914 \
-    --disable=R0915 \
-    --disable=R0913 \
-    "${script}"
-done
-
 # PEP8 tests
 # W503: Line break occurred before a binary operator
 # W504: Line break occurred after a binary operator
@@ -105,6 +70,39 @@ pylint \
   --disable=R0911 \
   --disable=R0904 \
   dotdrop/
+
+# check shell scripts
+# SC2002: Useless cat
+# SC2126: Consider using grep -c instead of grep|wc -l
+# SC2129: Consider using { cmd1; cmd2; } >> file instead of individual redirects
+# SC2181: Check exit code directly with e.g. 'if mycmd;', not indirectly with $?
+# SC1004: This backslash+linefeed is literal. Break outside single quotes if you just want to break the line
+echo "--------------------------------------"
+echo "checking shell scripts with shellcheck"
+find . -iname '*.sh' | while read -r script; do
+  echo "checking ${script}"
+  shellcheck -x \
+    -e SC2002 \
+    -e SC2126 \
+    -e SC2129 \
+    -e SC2181 \
+    -e SC1004 \
+    -e SC1117 \
+    -e SC2230 \
+    "${script}"
+done
+
+# check other python scripts
+echo "-----------------------------------------"
+echo "checking other python scripts with pylint"
+find . -name "*.py" -not -path "./dotdrop/*" | while read -r script; do
+  echo "checking ${script}"
+  pylint -sn \
+    --disable=R0914 \
+    --disable=R0915 \
+    --disable=R0913 \
+    "${script}"
+done
 
 echo "------------------------"
 echo "checking for more issues"
