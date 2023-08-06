c1b057da6bee5bc531d489479b45280438ebbf7b
deadc0de6 <info@deadc0de.ch>
2023-08-06 14:05:22 +0200

    update python version for #398

--- setup.py
+++ setup.py
@@ -37,11 +37,12 @@ setup(
     python_requires=REQUIRES_PYTHON,
     classifiers=[
             'Development Status :: 5 - Production/Stable',
-            'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9',
             'Programming Language :: Python :: 3.10',
+            'Programming Language :: Python :: 3.11',
+            'Programming Language :: Python :: 3.12',
             'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
           ],
 
