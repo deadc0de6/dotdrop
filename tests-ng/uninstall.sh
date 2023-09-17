7844997aa997d85cc992221c2ffff328f08490ad
deadc0de6 <info@deadc0de.ch>
2023-09-17 22:49:37 +0200

    tests

--- tests-ng/uninstall.sh
+++ tests-ng/uninstall.sh
@@ -25,102 +25,35 @@ echo -e "$(tput setaf 6)==> RUNNING $(basename "${BASH_SOURCE[0]}") <==$(tput sg
 ################################################################
 # this is the test
 ################################################################
+export DOTDROP_TEST_NG_UNINSTALL_DDPATH="${ddpath}"
+export DOTDROP_TEST_NG_UNINSTALL_BIN="${bin}"
+source "${cur}"/uninstall_
 
-# $1 pattern
-# $2 path
-grep_or_fail()
-{
-  grep "${1}" "${2}" >/dev/null 2>&1 || (echo "pattern \"${1}\" not found in ${2}" && exit 1)
-}
+export DOTDROP_TEST_NG_UNINSTALL_LINK_TYPE="nolink"
+echo "[+] testing uninstall link:${DOTDROP_TEST_NG_UNINSTALL_LINK_TYPE}..."
+uninstall_with_link
+echo "[+] uninstall link ${DOTDROP_TEST_NG_UNINSTALL_LINK_TYPE} OK"
 
-# dotdrop directory
-basedir=$(mktemp -d --suffix='-dotdrop-tests' || mktemp -d)
-mkdir -p "${basedir}"/dotfiles
-echo "[+] dotdrop dir: ${basedir}"
-echo "[+] dotpath dir: ${basedir}/dotfiles"
-tmpd=$(mktemp -d --suffix='-dotdrop-tests' || mktemp -d)
+export DOTDROP_TEST_NG_UNINSTALL_LINK_TYPE="absolute"
+echo "[+] testing uninstall link:${DOTDROP_TEST_NG_UNINSTALL_LINK_TYPE}..."
+uninstall_with_link
+echo "[+] uninstall link ${DOTDROP_TEST_NG_UNINSTALL_LINK_TYPE} OK"
 
-clear_on_exit "${basedir}"
-clear_on_exit "${tmpd}"
+export DOTDROP_TEST_NG_UNINSTALL_LINK_TYPE="relative"
+echo "[+] testing uninstall link:${DOTDROP_TEST_NG_UNINSTALL_LINK_TYPE}..."
+uninstall_with_link
+echo "[+] uninstall link ${DOTDROP_TEST_NG_UNINSTALL_LINK_TYPE} OK"
 
-echo "modified" > "${basedir}"/dotfiles/x
-mkdir -p "${basedir}"/dotfiles/y
-echo "modified" > "${basedir}"/dotfiles/y/file
+export DOTDROP_TEST_NG_UNINSTALL_LINK_TYPE="link_children"
+echo "[+] testing uninstall link:${DOTDROP_TEST_NG_UNINSTALL_LINK_TYPE}..."
+uninstall_with_link
+echo "[+] uninstall link ${DOTDROP_TEST_NG_UNINSTALL_LINK_TYPE} OK"
 
-# create the config file
-cfg="${basedir}/config.yaml"
-cat > "${cfg}" << _EOF
-config:
-  backup: true
-  create: true
-  dotpath: dotfiles
-dotfiles:
-  f_x:
-    src: x
-    dst: ${tmpd}/x
-  d_y:
-    src: y
-    dst: ${tmpd}/y
-profiles:
-  p1:
-    dotfiles:
-    - f_x
-    - d_y
-_EOF
-
-#########################
-## no original
-#########################
-
-# install
-echo "[+] install (1)"
-cd "${ddpath}" | ${bin} install -c "${cfg}" -f -p p1 --verbose | grep '^2 dotfile(s) installed.$'
-
-# tests
-[ ! -e "${tmpd}"/x ] && echo "f_x not installed" && exit 1
-[ ! -e "${tmpd}"/y/file ] && echo "d_y not installed" && exit 1
-grep_or_fail 'modified' "${tmpd}"/x
-grep_or_fail 'modified' "${tmpd}"/y/file
-
-# uninstall
-echo "[+] uninstall (1)"
-cd "${ddpath}" | ${bin} uninstall -c "${cfg}" -f -p p1 --verbose
-[ "$?" != "0" ] && exit 1
-
-# tests
-[ -e "${tmpd}"/x ] && echo "f_x not uninstalled" && exit 1
-[ -d "${tmpd}"/y ] && echo "d_y not uninstalled" && exit 1
-[ -e "${tmpd}"/y/file ] && echo "d_y file not uninstalled" && exit 1
-
-#########################
-## with original
-#########################
-echo 'original' > "${tmpd}"/x
-mkdir -p "${tmpd}"/y
-echo "original" > "${tmpd}"/y/file
-
-# install
-echo "[+] install (2)"
-cd "${ddpath}" | ${bin} install -c "${cfg}" -f -p p1 --verbose | grep '^2 dotfile(s) installed.$'
-
-# tests
-[ ! -e "${tmpd}"/x ] && echo "f_x not installed" && exit 1
-[ ! -d "${tmpd}"/y ] && echo "d_y not installed" && exit 1
-[ ! -e "${tmpd}"/y/file ] && echo "d_y file not installed" && exit 1
-grep_or_fail 'modified' "${tmpd}"/x
-grep_or_fail 'modified' "${tmpd}"/y/file
-
-# uninstall
-echo "[+] uninstall (2)"
-cd "${ddpath}" | ${bin} uninstall -c "${cfg}" -f -p p1 --verbose
-[ "$?" != "0" ] && exit 1
-
-# tests
-[ ! -e "${tmpd}"/x ] && echo "f_x backup not restored" && exit 1
-[ ! -d "${tmpd}"/y ] && echo "d_y backup not restored" && exit 1
-[ ! -e "${tmpd}"/y/file ] && echo "d_y backup not restored" && exit 1
-grep_or_fail 'original' "${tmpd}"/x
-grep_or_fail 'original' "${tmpd}"/y/file
+# TODO test
+# - symlink file (absolute, relative)
+# - symlink directory (absolute, relative, link_children)
+# - transformation
+# - template
 
 echo "OK"
 exit 0
